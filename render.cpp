e05877ab6821b22adaaf3cb5bf3f4be1085547ed
google.username <alibarker89@gmail.com>
2016-04-06 20:38:43 +0100

    with render.cpp added

--- render.cpp
+++ render.cpp
@@ -1,50 +1,65 @@
 /*
  * render.cpp
  *
- *  Created on: Oct 24, 2014
- *      Author: parallels
+ *  Created on: Apr 6, 2016
+ *      Author: Alistair Barker
  */
 
 
 #include <BeagleRT.h>
 #include <cmath>
 #include <Utilities.h>
- #include <Midi.h>
+#include <Midi.h>
+#include "Synth.h"
+#include "Scope.h"
+#include "MoogLadderFilter.h"
 
-// setup() is called once before the audio rendering starts.
-// Use it to perform any initialisation and allocation which is dependent
-// on the period size or sample rate.
-//
-// userData holds an opaque pointer to a data structure that was passed
-// in from the call to initAudio().
-//
-// Return true on success; returning false halts the program.
+int pot1 = 0;
+
+float freq;
+float inc = 1.00001;
+
+BLITOscillator osc;
+MoogLadderFilter filter;
+
+Scope scope;
+
 
 bool setup(BeagleRTContext *context, void *userData)
 {
+		scope.setup(context->audioSampleRate);
+
+	// set frequency
+	freq = 200;
+
+			filter.setCoefficients(filterTypeLowPass, 16000, 0.5, context->audioSampleRate);
+
+	osc.setFrequency(freq, context->audioSampleRate, oscTypeSaw);
+
 	return true;
 }
 
-// render() is called regularly at the highest priority by the audio engine.
-// Input and output are given from the audio hardware and the other
-// ADCs and DACs (if available). If only audio is available, numMatrixFrames
-// will be 0.
 
 void render(BeagleRTContext *context, void *userData)
 {
 	for(unsigned int n = 0; n < context->audioFrames; n++) {
-		float out = 0.8f * sinf(gPhase);
-		gPhase += 2.0 * M_PI * gFrequency * gInverseSampleRate;
-		if(gPhase > 2.0 * M_PI)
-			gPhase -= 2.0 * M_PI;
+
+		float freqIn = map(analogReadFrame(context, n/2, pot1), 0, 0.8, 200, 20000);
+
+
+
+		// rt_printf("Freq: %f\n", freqIn);
+		 filter.setCoefficients(filterTypeLowPass, freqIn, 0.5, context->audioSampleRate);
+		// freq *= inc;
+
+		// if (freq >= 18000 || freq <= 20 )
+			// inc = 1/inc;
+
+		float out = osc.tick();
+
+		out = filter.processSample(out);
 
 		for(unsigned int channel = 0; channel < context->audioChannels; channel++) {
-			// Two equivalent ways to write this code
-
-			// The long way, using the buffers directly:
-			// context->audioOut[n * context->audioChannels + channel] = out;
-
-			// Or using the macros:
 			audioWriteFrame(context, n, channel, out);
 		}
 	}
@@ -53,7 +68,7 @@ void render(BeagleRTContext *context, void *userData)
 // cleanup() is called once at the end, after the audio has stopped.
 // Release any resources that were allocated in setup().
 
+
 void cleanup(BeagleRTContext *context, void *userData)
 {
-
 }
