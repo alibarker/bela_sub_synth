002970b381dfc82bad5022639e1868c543411178
google.username <alibarker89@gmail.com>
2016-04-08 17:20:45 +0100

    Envelope Added

---
+++ Envelope.h
@@ -0,0 +1,115 @@
+/*
+ *   Envelope.h
+ *
+ *  Created on: Apr 6, 2016
+ *      Author: Alistair Barker
+ */
+
+enum State {
+	stateOff= 0,
+	stateAttack,
+	stateDecay,
+	stateSustain,
+	stateRelease
+};
+
+
+class Envelope
+{
+public:
+	Envelope(float Fs)
+	{
+
+		state = stateOff;
+		sampleRate = Fs;
+		setParameters(0.1, 0.1, 0.8, 0.1);
+		currentAmplitude = 0.0;
+
+		rt_printf("Fs: %f\n", sampleRate);
+	}
+	~Envelope(){}
+
+	float tick();
+
+	void setParameters(float a, float d, float s, float r)
+	{
+		// attack is linear, decay/release is exponential
+		attackInc = 1/(a*sampleRate);
+		decayInc = pow(exp(-1/d), 1/sampleRate);
+		sustain = s;
+		releaseInc = pow(exp(-1/r), 1/sampleRate);
+
+rt_printf("sampleRate: %f\t attackInc: %f\t, decayInc: %f\t, sustain: %f\t, releaseInc: %f\n", sampleRate, attackInc, decayInc, sustain, releaseInc);
+
+	}
+
+	void startNote()
+	{
+		state = stateAttack;
+		rt_printf("Note on\n");
+	}
+	void releaseNote()
+	{
+		state = stateRelease;
+				rt_printf("Note off\n");
+ 
+	}
+
+	State getState()
+	{
+		return state;
+	}
+	
+private:
+	State state;
+
+	double attackInc, decayInc, sustain, releaseInc;
+
+	float currentAmplitude;
+
+	float sampleRate;
+
+};
+
+float Envelope::tick()
+{
+	switch(state) {
+		case stateOff:
+			currentAmplitude = 0.0;
+			break;
+
+		case stateAttack:
+			currentAmplitude += attackInc;
+			if (currentAmplitude >= 1){
+				state = stateDecay;
+				rt_printf("Decay\n");
+			}
+			break;
+
+		case stateDecay:
+			currentAmplitude *= decayInc;
+			if (currentAmplitude <= sustain){
+				state = stateSustain;
+				rt_printf("Sustain\n");
+			}
+			break;
+
+		case stateRelease:
+			currentAmplitude *= releaseInc;
+			if (currentAmplitude <= 0.0001)
+			{
+				currentAmplitude = 0.0;
+				state = stateOff;
+				rt_printf("End\n");
+			}
+			break;
+
+		case stateSustain:
+			currentAmplitude = sustain;
+			break;
+
+		}
+
+	return currentAmplitude;
+
+};
--- render.cpp
+++ render.cpp
@@ -13,29 +13,39 @@
 #include "Synth.h"
 #include "Scope.h"
 #include "MoogLadderFilter.h"
+ #include "Envelope.h"
+
+Envelope* env;
 
 int pot1 = 0;
+int button1 = P9_12;
+int pot2 = 1;
 
-float freq;
+
+float prevFreqIn;
 float inc = 1.00001;
+
+int button1PrevState;
 
 BLITOscillator osc;
 MoogLadderFilter filter;
 
-Scope scope;
+// Scope scope;
 
 
 bool setup(BeagleRTContext *context, void *userData)
 {
-		scope.setup(context->audioSampleRate);
+		// scope.setup(context->audioSampleRate);
 
 	// set frequency
-	freq = 200;
 
 			filter.setCoefficients(filterTypeLowPass, 16000, 0.5, context->audioSampleRate);
 
-	osc.setFrequency(freq, context->audioSampleRate, oscTypeSaw);
+	osc.setFrequency(200, context->audioSampleRate, oscTypeSaw);
 
+	env = new Envelope(context->audioSampleRate);
+
+	pinModeFrame(context, 0, button1, INPUT);
 	return true;
 }
 
@@ -44,20 +54,38 @@ void render(BeagleRTContext *context, void *userData)
 {
 	for(unsigned int n = 0; n < context->audioFrames; n++) {
 
-		float freqIn = map(analogReadFrame(context, n/2, pot1), 0, 0.8, 200, 20000);
+		float freqIn = map(analogReadFrame(context, n/2, pot1), 0, 0.85, 200, 15000);
+		float resIn = map(analogReadFrame(context, n/2, pot2), 0, 0.43, 0, 1.5);
+
+		filter.setCoefficients(filterTypeLowPass, 0.5*(freqIn + prevFreqIn), 1, context->audioSampleRate);
+
+		prevFreqIn = freqIn;
 
 
+		int button1state = digitalReadFrame(context, n, button1);
 
-		// rt_printf("Freq: %f\n", freqIn);
-		 filter.setCoefficients(filterTypeLowPass, freqIn, 0.5, context->audioSampleRate);
-		// freq *= inc;
+		if (button1state == 0 && button1PrevState == 1)
+		{
+			env->startNote();
+		} else if (button1state == 1 && button1PrevState == 0)
+		{
+			env->releaseNote();
+		}
+		// rt_printf("button1state:\t%d\n", button1state);
+		button1PrevState = button1state;
 
-		// if (freq >= 18000 || freq <= 20 )
-			// inc = 1/inc;
+		rt_printf("Freq: %f\n", freqIn);
+		// // freq *= inc;
+
+		// // if (freq >= 18000 || freq <= 20 )
+		// 	// inc = 1/inc;
 
 		float out = osc.tick();
+		float envAmp = env->tick();
 
-		out = filter.processSample(out);
+		// rt_printf("amp: %f\n", envAmp);
+
+		out = filter.processSample(out) * envAmp * 0.125;
 
 		for(unsigned int channel = 0; channel < context->audioChannels; channel++) {
 			audioWriteFrame(context, n, channel, out);
@@ -71,4 +99,5 @@ void render(BeagleRTContext *context, void *userData)
 
 void cleanup(BeagleRTContext *context, void *userData)
 {
+	delete env;
 }
